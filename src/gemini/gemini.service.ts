7af4f69e51eb4fb6cf3aa2e0d1a8f7639bb5fe1f
Going <goinghiggvp@gmail.com>
2025-10-05 01:20:45 +0300

    format

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,24 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { GenerativeModel, GoogleGenerativeAI } from "@google/generative-ai";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
 
 @Injectable()
 export class GeminiService {
-    private model: GenerativeModel
-    
-    constructor(configService: ConfigService) {
-        const apiKey = configService.get('GEMINI_API_KEY');
-        const genAI = new GoogleGenerativeAI(apiKey)
+  private model: GenerativeModel;
 
-        this.model = genAI.getGenerativeModel({
-            model: 'gemini-2.0-flash',
-        })
-    }
+  constructor(configService: ConfigService) {
+    const apiKey = configService.get('GEMINI_API_KEY');
+    const genAI = new GoogleGenerativeAI(apiKey);
 
+    this.model = genAI.getGenerativeModel({
+      model: 'gemini-2.0-flash',
+    });
+  }
 
-    public async generate(prompt: string): Promise<string> {
-        const res = await this.model.generateContent(prompt)
-        const text = res.response.text()
-        return text
-    }
+  public async generate(prompt: string): Promise<string> {
+      const res = await this.model.generateContent(prompt);
+      const text = res.response.text();
+      return text;
+  }
 }
