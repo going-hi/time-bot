c3efe8c913ee653e35e05cbfe20d49c61c22e979
Going <goinghiggvp@gmail.com>
2025-10-05 01:19:47 +0300

    update set city command

--- src/bot/commands/set-city.command.ts
+++ src/bot/commands/set-city.command.ts
@@ -1,18 +1,50 @@
-import { AbstractCommand } from "./abstract.command";
-import { Context, CommandContext } from "grammy";
-
-export const cities: string[] = ['Москва', 'Лондон', 'Нью-Йорк', 'Астана'];
+import { AbstractCommand } from './abstract.command';
+import { Context, CommandContext } from 'grammy';
 
 export class SetCityCommand extends AbstractCommand {
-    public commandName = "setcity"
-    public commandDescription = `Добавляет город для отслеживания времени ex: ${this.commandName} Москва`
+  public commandName = 'setcity';
+  public commandDescription = `Добавляет город для отслеживания времени ex: ${this.commandName} Москва`;
 
-    public isMenuCommand = true
+  public isMenuCommand = true;
 
-    async execute(ctx: CommandContext<Context>): Promise<void> {
-        const text = ctx.message?.text || ""
-        const params = this.parseArgs(text)
-        cities.push(...params)
-        await ctx.reply(`Города для отслеживания времени: ${cities.join(", ")}`)
+  async execute(ctx: CommandContext<Context>): Promise<void> {
+    const text = ctx.message?.text || '';
+    const params = this.parseArgs(text);
+    const chatId = ctx.message?.chat.id!;
+
+    const cities = await this.getSaveCities(chatId);
+
+    const messageCities = cities.length
+      ? `Города доступные сейчас ${cities.join(', ')}`
+      : 'У вас пока не добавлены города и страны для отслеживания';
+
+    if (!params.length) {
+      await ctx.reply(
+        `Укажите города для добавления отслеживания, ${messageCities}`,
+      );
+      return;
     }
+    cities.push(...params);
+
+    const filterCities = Array.from(new Set(cities))
+
+    await this.saveCities(chatId, filterCities)
+    await ctx.reply(`Города для отслеживания времени: ${filterCities.join(', ')}`);
+  }
+
+  private async saveCities(chatId: number, cities: string[]) {
+    const key = `chatik:${chatId}:cities`;
+    await this.cacheSqliteService.set(key, cities.join(','));
+  }
+
+  private async getSaveCities(chatId: number): Promise<string[]> {
+    const citiesRaw = await this.cacheSqliteService.get(
+      `chatik:${chatId}:cities`,
+    );
+    if (!citiesRaw) {
+      return [];
+    }
+
+    return citiesRaw.split(',');
+  }
 }
