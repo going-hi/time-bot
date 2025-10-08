83165d5f5eb20268681d1090d239d35ee0c090f8
Going <goinghiggvp@gmail.com>
2025-10-08 23:10:23 +0300

    add id

--- src/bot/commands/ping.command.ts
+++ src/bot/commands/ping.command.ts
@@ -8,6 +8,8 @@ export class PingCommand extends AbstractCommand {
     public isMenuCommand = true
 
     async execute(ctx: CommandContext<Context>): Promise<void> {
+        console.log(ctx.message?.chat, "HUI")
+
         await ctx.reply("pong pong pong pong!!!", {
             reply_parameters: { message_id: ctx.msg.message_id },
         })
